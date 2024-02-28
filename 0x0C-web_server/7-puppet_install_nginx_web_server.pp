7dfc1a39a12116258e885c447ebed0ea6ad4499a
Erick Olando <Opiyoerick1998@gmail.com>
2024-02-28 14:22:54 -0800

    Web-server_02

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,59 +1,24 @@
-# Installs and configures Nginx web server.
+# Setup New Ubuntu server with nginx
 
-$doc_root = '/var/www/html'
-$server_block = '/etc/nginx/sites-available'
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
 
 package { 'nginx':
-  ensure   => 'installed',
-  provider => 'apt',
-  before   => Exec['allow nginx']
+	ensure => 'installed',
+	require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
 service {'nginx':
-  ensure => 'running',
-  enable => 'true'
-}
-
-exec {'apt-get update':
-  command => '/usr/bin/apt-get update -y',
-  before  => Package['nginx']
-}
-
-service { 'ufw':
-  ensure => 'running',
-  enable => 'true'
-}
-
-exec {'allow nginx':
-  command => '/usr/bin/sudo ufw allow "Nginx HTTP"',
-  require => Service['ufw']
-}
-
-file {'index.html':
-  ensure  => 'present',
-  path    => "${doc_root}/index.html",
-  content => "Hello World!\n",
-  notify  => Service['nginx'],
-  require => Package['nginx']
-}
-
-file {'custom_404':
-  ensure  => 'present',
-  path    => "${doc_root}/custom_404",
-  content => "Ceci n\'est pas une page\n",
-  notify  => Service['nginx'],
-  require => Package['nginx']
-}
-
-file {'default':
-  ensure  => 'present',
-  path    => "${server_block}/default",
-  backup  => 'true',
-  require => Package['nginx']
-}
-
-exec {'sed':
-  command => '/usr/bin/sed -i "s%server_name _;%server_name _;\n\trewrite ^/redirect_me https://pinterest.com permanent;\n\terror_page 404 /custom_404;\n\tlocation = /custom_404 {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}%" /etc/nginx/sites-available/default',
-  require => File['default'],
-  notify  => Service['nginx']
+	ensure => running,
+	require => Package['nginx']
 }
